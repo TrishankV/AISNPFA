14cef57fdcdbb352fa8fb5cb26faeb96072a8b90
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-09-22 18:28:52 +0530

    Update connection.py

--- Final/Python_Connections/connection.py
+++ Final/Python_Connections/connection.py
@@ -1,6 +1,5 @@
 import socket
 import time
-import random
 
 #goal state
 
@@ -23,19 +22,41 @@ Location = {'A':[-9.3,1.25,-4.94],
             'L':[0.75,1.25,9.55],
             'M':[9.34,1.25,4.46],
             'N':[9.3,1.25,9.55]}
-# print(Location['A'])
-try:
-    address = input("Enter address : ")
-    key = address.capitalize()
-    print(Location[key])
-except KeyError:
-    print("Please check the address")
+# # print(Location['A'])
+def simulate(address,pickup):
+    try:
+        # address = input("Enter address : ")
+        key = pickup.capitalize()
+        print( "Source ",Location[key])
+        des = address.capitalize()
+        print( "Destination : ",Location[des])
+        connect(Location[key])
+        time.sleep(20)
+        dest(Location[des])
+        return True
+    except KeyError:
+        print("Please check the address")
+        return False
 
-startPos = Location[key]
-posString = ','.join(map(str, startPos)) #Converting Vector3 to a string, example "0,0,0"
-print(posString)
+def connect (src):
+    startPos = [-9.3,1.25,-4.94]
+    if startPos != src :
+        startPos = src
+        posString = ','.join(map(str, startPos)) #Converting Vector3 to a string, example "0,0,0"
+        print(posString)
+        sock.sendall(posString.encode("UTF-8")) #Converting string to Byte, and sending it to C#
+        receivedData = sock.recv(1024).decode("UTF-8") #receiveing data in Byte fron C#, and converting it to String
+        print(receivedData)
+            
+
+def dest (des):
+    startPos = des
+    posString = ','.join(map(str, startPos)) #Converting Vector3 to a string, example "0,0,0"
+    print(posString)
+    sock.sendall(posString.encode("UTF-8")) #Converting string to Byte, and sending it to C#
+    receivedData = sock.recv(1024).decode("UTF-8") #receiveing data in Byte fron C#, and converting it to String
+    print(receivedData)
+    print("reached Destination")
+ 
 
 
-sock.sendall(posString.encode("UTF-8")) #Converting string to Byte, and sending it to C#
-receivedData = sock.recv(1024).decode("UTF-8") #receiveing data in Byte fron C#, and converting it to String
-print(receivedData)
