3349d34fd33c22be769e4c1ae71c9b218ada20eb
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-09-28 12:59:51 +0530

    Update send_email.py

--- Final/Python_Connections/send_email.py
+++ Final/Python_Connections/send_email.py
@@ -3,37 +3,38 @@
 import smtplib
 from email.mime.text import MIMEText
 
-def send_email(name_contact,email,phone_contact,message):
+def send_email(name_contact,sender_email,phone_contact,message):
 
     # Set up the SMTP server
-    sender_email="Email"
-    sender_password="Password"
-    message = "Name : "+ name_contact + "\nEmail of Sender : " + email +"\nPhone : "+ phone_contact + "\n Querey : " + message
+    print(name_contact)
+    print(sender_email)
+    print(phone_contact)
+    print(message)
+    message = "Name : "+ name_contact + "\nEmail of Sender : " + sender_email +"\nPhone : "+ phone_contact + "\n Querey : " + message
     try: 
-        smtp_server = "smtp.gmail.com"
-        smtp_port = 587
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(sender_email, sender_password)
-
-        # Create the email message
+        sender = "query_from_user_simulation@outlook.com"
+        recipient = "query_from_user_simulation@outlook.com"
         email_message = MIMEText(message)
-        email_message["Subject"] = "Querey from a Sender"
-        print("Subject :Querey from a Sender ")
-        email_message["From"] = sender_email
-        email_message["To"] = email
+        email_message["Subject"] = name_contact + " trying to Contact"
+        email_message["From"] = sender
+        email_message["To"] = recipient
+        print("email drafted.....")
 
-    # Send the email
-        server.send_message(email_message)
-        server.quit()
+        smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
+        smtp.starttls()
+        smtp.login(sender, "Argentavious")
+        smtp.sendmail(sender, recipient, email_message.as_string())
+        print("email sent.....")
+        smtp.quit()
+
     except:
         print("Mail not send as it was blocked due to security reasons or senders email and password is missing")
 
 # Example usage
 if __name__ == "__main__" :
-    sender_email = "youremail"
-    sender_password = "pass"
-    recipient_email = "tvashikar@gmail.com"
-    subject = "Task Completed"
-    message = "Hello, Your task has been completed successfully!"
-    send_email(sender_email, sender_password, recipient_email, subject, message)
+    sender_email = "vedanranade2612@gmail.com"
+    # sender_password = "pass"
+    name = "Vedant Ranade"
+    Phone = "9324526912"
+    message = "I have a query is this mail working in the miniproject presentation???"
+    send_email(name,sender_email,Phone,message)
